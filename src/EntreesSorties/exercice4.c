ad10effefddc0f3c87b4b5e0e9f8fd7a26eb970a
ApsilonBlan <advin.depardieu@univ-tlse3.fr>
2019-10-03 20:29:46 +0000

    Fin 3eme séance

--- src/EntreesSorties/exercice4.c
+++ src/EntreesSorties/exercice4.c
@@ -1,2 +1,32 @@
 #include <stdio.h>
 
+/*Les 2 morceaux de pogramme suivant font la même chose :
+printf("Bonjour.\n");
+
+char * hello="Bonjour";
+printf("%s.\n", hello);*/
+
+int main(int argc, char * argv[])
+{
+    /*printf("%s %s %s\n", argv[0], argv[1], argv[2]);*/
+   
+    /*printf("%d\n", arg)*/
+   
+   /*argc = nombre de mots
+    boucle pour afficher tout ce qui est tapé:*/
+    int i;
+    for (i=0; i<argc; i++)
+    {
+        printf("%s\n", argv[i]);
+    }
+    
+    for (i=argc+1; i<10; i++)
+    {
+        printf("%s\n", argv[i]);
+    }
+    
+    printf("\n%s\n", argv[80]); /*"Segmentation_fault (core dumped)" ou "(null)"*/
+    
+}
+
+/*Taper "echo $LANG" dans le terminal entraine l'affichage de "en_US.UTF-8"*/
--- src/EntreesSorties/exercice5_2.c
+++ src/EntreesSorties/exercice5_2.c
@@ -2,9 +2,19 @@
 
 int main (int argc, char * argv[])
 {
-    int e=0;
-    printf("Bonjour, à qui ai-je l'honneur ?\n");
-    scanf("Bonjour, je suis %d\n", &e);
-    printf("Bonjour %d\n",e);
+    printf("Ecrivez : Bonjour, monde !\n");
+    scanf("Bonjour, monde !\n");
+    if(getchar()=='\n')
+    {
+        printf("Je vous ai compris !\n");
+    }
     
+    /*int e;
+    printf("Bonjour, à qui ai-je l'honneur ?\n");
+    scanf("Bonjour, je suis %d", &e); 
+    getchar();
+    printf("Bonjour %d\n",e);*/
+    return 0;
+   /*Attention à ne pas mettre de \n dans un scanf, 
+   il est considéré comme partie intégrante de l'entier autrement*/
 }
